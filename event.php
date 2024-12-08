ece6033c5f9997ea8c82aafdded9188c0f9a592a
Evan Hamou <EvanHamou@mail.adelphi.edu>
2024-12-08 12:14:44 -0500

    Finished Instructor

--- event.php
+++ event.php
@@ -44,4 +44,3 @@ if(isset($_POST['event']))
 
 
 ?>
-<!-- Create Event PHP Completed-->
--- instructor.php
+++ instructor.php
@@ -1,5 +1,47 @@
 <?php
 
-echo "This is my instructor page"
+//database info.
+$servername="localhost";
+$username="root";
+$password= "";
+$database= "SEProject2";
+
+
+//creating connection to the database
+$conn= new mysqli($servername,$username, $password, $database);
+
+if(isset($_POST['difficulty']))
+{
+ $ClassID=$_POST['ClassID'];
+ $Difficulty=$_POST['Difficulty'];
+
+ //query to check if class is in database
+ $checkIfClassIDExistQuery="SELECT * FROM Classes WHERE ClassID='$ClassID'";
+ $result=$conn->query($checkIfClassIDExistQuery);
+
+
+    if(($result->num_rows>0))
+    {
+
+    $addDifficultyQuery="UPDATE Classes SET Difficulty = '$Difficulty' WHERE ClassID = '$ClassID'";
+    $conn->query($addDifficultyQuery);
+    echo "The Difficulty has been added to the class.";
+    
+    }
+    else
+    {
+    echo "The Class doesn't exist. Please enter a valid class ID.".mysqli_error($conn);
+    
+    }
+
+ 
+
+    $conn->close();
+ }
+
+
+    
+
+
 
 ?>
